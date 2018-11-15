5bcfa57235cd5a7ff46fbfde7237d7d9fb8cff11
Kadlec, Jakub <kadlej26@fel.cvut.cz>
2018-11-15 22:35:16 +0100

    + working

--- HW04/main.c
+++ HW04/main.c
@@ -7,59 +7,53 @@ char sign;
 int * pMat0;
 int * pMat1;
 
-void add (int *mat0, int *mat1, int radku, int sloupcu){
-  printf ("%d %d\n", radku, sloupcu);
-  printf ("mat0[0] = %d\n", *mat0);
-  //printf ();
-  for (int r = 0; r < radku*sloupcu; r++){
-    if (r % sloupcu == 0){
-      printf ("\n");
-    }
-    printf(" %d", (*(mat0 + r) + *(mat1 + r)));
-
-  }
-  printf ("\n");
-}
-
-void fillMatrix (int *mat, int pocPrvku){
-  int in;
-  for (int i = 0; i < pocPrvku; i++){
-    scanf ("%d", &in);
-    *(mat+i) = in;
-  }
-  for (int i = 0; i < pocPrvku; i++){
-    printf ("matx[%d] = %d\n", i, *(mat+i));
-  }
-}
-
 unsigned char input (){
   unsigned char iter = 0;
-  while (iter < 2){
-    if (2 != scanf("%d %d", &rozm[iter][0], &rozm[iter][1])){
-      fprintf(stderr, "Error: Chybny vstup!\n");
-      return 101;
-    }
-    if (iter == 0){
-      int matrix0 [rozm[iter][0]][rozm[iter][1]];
-      pMat0 = *matrix0;
-      fillMatrix(pMat0, sizeof (matrix0)/4);
-    }
-    else {
-      int matrix1 [rozm[iter][0]][rozm[iter][1]];
-      pMat1 = *matrix1;
-      fillMatrix(pMat1, sizeof (matrix1)/4);
-    }
-    if (iter == 0){
-      if (1 != scanf(" %c", &sign)){
-        fprintf(stderr, "Error: Chybny vstup!\n");
-        return 101;
-      }
-    }
-    iter ++;
+  int in;
+
+  if (2 != scanf("%d %d", &rozm[iter][0], &rozm[iter][1])){
+    fprintf(stderr, "Error: Chybny vstup!\n");
+    return 101;
   }
+
+  int matrix0 [rozm[iter][0]][rozm[iter][1]];
+  pMat0 = *matrix0;
+  for (int i = 0; i < sizeof (matrix0)/4; i++){
+    scanf ("%d", &in);
+    *(pMat0+i) = in;
+  }
+
+  if (1 != scanf(" %c", &sign)){
+    fprintf(stderr, "Error: Chybny vstup!\n");
+    return 101;
+  }
+
+  iter ++;
+
+  if (2 != scanf("%d %d", &rozm[iter][0], &rozm[iter][1])){
+    fprintf(stderr, "Error: Chybny vstup!\n");
+    return 101;
+  }
+
+  int matrix1 [rozm[iter][0]][rozm[iter][1]];
+  pMat1 = *matrix1;
+
+  for (int i = 0; i < sizeof (matrix1)/4; i++){
+    scanf ("%d", &in);
+    *(pMat1+i) = in;
+  }
+
   if (sign == '+' && rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
-    add(pMat0, pMat1, rozm[0][0], rozm[0][1]);
+    printf ("%d %d\n", rozm[0][0], rozm[0][1]);
+    for (int r = 0; r < rozm[0][0]*rozm[0][1]; r++){
+      printf("%d", (*(pMat0 + r) + *(pMat1 + r)));
+      if (r % (rozm[0][1] - 1) == 0 && r != 0){
+        printf ("\n");
+      }
+      else printf (" ");
+    }
   }
+
   else if (sign == '-' && rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
     printf ("%c", sign);
   }
