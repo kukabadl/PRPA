3b74e554e3f4108b34b62d5bbf73a058b29d68f4
Kadlec, Jakub <kadlej26@fel.cvut.cz>
2018-11-16 01:21:04 +0100

    + working well

--- HW04/main.c
+++ HW04/main.c
@@ -13,7 +13,7 @@ unsigned char input (){
 
   if (2 != scanf("%d %d", &rozm[iter][0], &rozm[iter][1])){
     fprintf(stderr, "Error: Chybny vstup!\n");
-    return 101;
+    return 100;
   }
 
   int matrix0 [rozm[iter][0]][rozm[iter][1]];
@@ -25,14 +25,14 @@ unsigned char input (){
 
   if (1 != scanf(" %c", &sign)){
     fprintf(stderr, "Error: Chybny vstup!\n");
-    return 101;
+    return 100;
   }
 
   iter ++;
 
   if (2 != scanf("%d %d", &rozm[iter][0], &rozm[iter][1])){
     fprintf(stderr, "Error: Chybny vstup!\n");
-    return 101;
+    return 100;
   }
 
   int matrix1 [rozm[iter][0]][rozm[iter][1]];
@@ -43,30 +43,65 @@ unsigned char input (){
     *(pMat1+i) = in;
   }
 
-  if (sign == '+' && rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
-    printf ("%d %d\n", rozm[0][0], rozm[0][1]);
-    //printf ("sloupcu: %d\n", rozm[0][1]);
-
-    for (int r = 0; r < rozm[0][0]*rozm[0][1]; r++){
-      printf("%d", (*(pMat0 + r) + *(pMat1 + r)));
-      //printf ("\n r= %d\n", (r+1)% (rozm[0][1]));
-
-      if ((r + 1) % (rozm[0][1]) == 0){
-        printf ("\n");
+  if (sign == '+'){
+    if (rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
+      printf ("%d %d\n", rozm[0][0], rozm[0][1]);
+      for (int r = 0; r < rozm[0][0]*rozm[0][1]; r++){
+        printf("%d", (*(pMat0 + r) + *(pMat1 + r)));
+        if ((r + 1) % (rozm[0][1]) == 0){
+          printf ("\n");
+        }
+        else printf (" ");
       }
-      else printf (" ");
+    }
+    else {
+      fprintf(stderr, "Error: Chybny vstup!\n");
+      return 100;
     }
   }
 
-  else if (sign == '-' && rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
-    printf ("%c", sign);
+  else if (sign == '-'){
+    if (rozm[0][0] == rozm[1][0] && rozm[0][1] == rozm[1][1]){
+      printf ("%d %d\n", rozm[0][0], rozm[0][1]);
+      for (int r = 0; r < rozm[0][0]*rozm[0][1]; r++){
+        printf("%d", (*(pMat0 + r) - *(pMat1 + r)));
+        if ((r + 1) % (rozm[0][1]) == 0){
+          printf ("\n");
+        }
+        else printf (" ");
+      }
+    }
+    else {
+      fprintf(stderr, "Error: Chybny vstup!\n");
+      return 100;
+    }
   }
   else if (sign == '*'){
-    printf ("%c", sign);
+    if (rozm[0][1] == rozm[1][0] && rozm [0][0] == rozm [1][1]){
+      printf ("%d %d\n", rozm[0][0], rozm[1][1]);   //rozm[1][1] >> pocet sloupcu
+      for (int i = 0; i < rozm [0][0]; i++){
+        for (int q = 0; q < rozm [1][1]; q++){
+          int mezivypocet = 0;
+          for (int r = 0; r < rozm[1][0]; r++){
+            //printf ("%d\n", *(pMat0 + r + i*rozm[0][1])*(*(pMat1 + q + r*rozm[1][1])));
+            mezivypocet += *(pMat0 + r + i*rozm[0][1])*(*(pMat1 + q + r*rozm[1][1]));
+          }
+          printf("%d", mezivypocet);
+          if (q == rozm [1][1] - 1){
+            printf ("\n");
+          }
+          else printf (" ");
+        }
+      }
+    }
+    else {
+      fprintf(stderr, "Error: Chybny vstup!\n");
+      return 100;
+    }
   }
   else {
     fprintf(stderr, "Error: Chybny vstup!\n");
-    return 101;
+    return 100;
   }
   return 0;
 }
