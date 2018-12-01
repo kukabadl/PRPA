69fc407f28ace8ca1b4d9a3ec2442e6152134807
Kadlec, Jakub <kadlej26@fel.cvut.cz>
2018-12-01 01:41:27 +0100

    Fully working hopefelly, not Brute formatted yet.

--- HW05/main.c
+++ HW05/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 int numOfColumns;
 int numCount = 0;
 int * pMatrix;
 int max0Size;
 int max1Size;
 int maxInInterval = 0;
-
-double log(double x);
-
+int roun(double x){
+  int rounded = (int) x;
+  while (x >= 1) x--;
+  if (x > 0.5f) return rounded + 1;
+  else return rounded;
+}
 int cmp (const void * cislo0, const void * cislo1) {
    return (*(int *) cislo0 - *(int *) cislo1);
 }
-
 int size (int size){
-	//printf ("size = %d ", size);
 	int log = 0;
 	if (size < 0){
 		log = 1;
@@ -26,23 +26,21 @@ int size (int size){
 		size /= 10;
 		log++;
 	}
-	//printf ("log = %d\n", log + 2);
-	return log + 2;
+	return log;
 }
-
 int sortIt () {
 	float interval;
 	if (1 != scanf("%d", &numOfColumns)) return 100;
 	int inInterval [numOfColumns];
-	pMatrix = (int*) malloc(101 * sizeof(int));
-	for (;scanf("%d", (pMatrix + numCount)) != EOF; numCount++){
-		if ((numCount % 100) == 0){
-			pMatrix = realloc(pMatrix, (numCount + 100) * sizeof(int));
+	pMatrix = (int*) malloc(1002 * sizeof(int));
+	for (;scanf("%d", (pMatrix + numCount)) > 0; numCount++){
+		if ((numCount % 1000) == 0){
+			pMatrix = realloc(pMatrix, (numCount + 1 + 1000) * sizeof(int));
 		}
 	}
-	qsort (pMatrix, numCount, sizeof(int), cmp);
+  qsort (pMatrix, numCount, sizeof(int), cmp);
 
-	printf("Median: %.2f\n",(numCount%2)? (*(pMatrix + numCount/2)):(*(pMatrix + numCount/2 - 1) + *(pMatrix + numCount/2))/(float)2);
+	printf("Median %.2f\n",(numCount%2)? (*(pMatrix + numCount/2)):(*(pMatrix + numCount/2 - 1) + *(pMatrix + numCount/2))/(float)2);
 	printf("Number count: %d\n", numCount);
 	numCount --;
 	printf("Min: %d\n", *(pMatrix));
@@ -51,18 +49,18 @@ int sortIt () {
 	interval = (float)(fabs((*(pMatrix + numCount) - *(pMatrix))/(float)numOfColumns));
 
 	for (int s = 0; s < numOfColumns; s++){					//najde jaky interval bude mit pri vypsani nejvetsi delku
-		if(size((int)((*(pMatrix)) + s * interval)) > max0Size){
+    if(size((int)((*(pMatrix)) + s * interval)) > max0Size){
 			max0Size = size((int)((*(pMatrix)) + s * interval));
 		}
-		if(size((int)((*(pMatrix)) + s * interval)) > max1Size){
+		if(size((int)((*(pMatrix)) + (s + 1) * interval)) > max1Size){
 			max1Size = size((int)((*(pMatrix)) + (s + 1) * interval));
 		}
 	}
 	int iter = 0;
 
-	for (int r = 0; r <= numOfColumns; r++){
+	for (int r = 0; r < numOfColumns; r++){
 		inInterval[r] = 0;
-		for (; *(pMatrix + iter) < ((*(pMatrix)) + (1 + r) * interval); iter ++){
+		for (; *(pMatrix + iter) <= ((*(pMatrix)) + (1 + r) * interval); iter ++){
 			inInterval[r]++;
 			if (iter + 1 > numCount){
 				iter++;
@@ -71,16 +69,24 @@ int sortIt () {
 			if (inInterval[r] > maxInInterval) maxInInterval = inInterval[r];
 		}
 	}
-
 	for (int r = 0; r < numOfColumns; r++){
-		for (int space = 0; space < max0Size - size ((*(pMatrix)) + r * interval); space++){
+
+		for (int space = 0; space < max0Size - size((*(pMatrix)) + r * interval); space++){
 			printf(" ");
 		}
-		printf (" %.1f -", (*(pMatrix)) + r * interval);
-		for (int space = 0; space < max0Size - size ((*(pMatrix)) + (r + 1) * interval); space++){
+
+		printf ("%3.1f -", (*(pMatrix)) + r * interval);
+
+		for (int space = 0; space < max1Size - size((*(pMatrix)) + (r + 1) * interval); space++){
 			printf(" ");
 		}
-		printf ("  %.1f|\n",(*(pMatrix)) + (r + 1)*interval);
+
+		printf (" %3.1f|",(*(pMatrix)) + (r + 1)*interval);
+    int temp = (int)(inInterval[r]*25/(float)maxInInterval);
+    for(int i = 0; i < temp; i++){
+      printf("=");
+    }
+    printf("\n");
 	}
 	free (pMatrix);
 	return 0;
@@ -88,7 +94,7 @@ int sortIt () {
 
 int main (){
 	if (sortIt() == 100){
-		printf("Histogram size error\n");
+		fprintf(stderr, "Histogram size error\n");
 		return 100;
 	}
 	else return 0;
