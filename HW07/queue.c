4665a827e382bb24cec7390e29547203521e6a87
Kadlec, Jakub <kadlej26@fel.cvut.cz>
2018-12-28 19:54:15 +0100

    First attempt

--- HW07/queue.c
+++ HW07/queue.c
@@ -6,13 +6,15 @@ queue_t * create_queue(int capacity){
   queue_t * pFirst;
   queue_t * pCurrentStruct = (queue_t *)malloc(sizeof(queue_t));
   pFirst = pCurrentStruct;
-  if (capacity < 1){
+  if (capacity > 1){
     for (int i = 0; i < (capacity - 1); i++){
       pCurrentStruct->pNext = (queue_t *)malloc(sizeof(queue_t));
+      pCurrentStruct->pData = NULL;
       pCurrentStruct = pCurrentStruct->pNext;
     }
   }
   pCurrentStruct->pNext = NULL;
+  pCurrentStruct->pData = NULL;
   pHead = pFirst;
   return pFirst;
 }
@@ -28,7 +30,36 @@ bool push_to_queue (queue_t *queue, void *data)
 }
 
 int get_queue_size(queue_t *queue){
-  
+  int numOfElements = 0;
+  if (queue =! NULL){
+    queue_t * Temp = queue;
+    numOfElements = 1;
+    while (Temp->pNext != NULL){
+      numOfElements++;
+      Temp = Temp->pNext;
+    }
+  }
+  return numOfElements;
+}
+
+void* get_from_queue(queue_t *queue, int idx){
+  queue_t * Temp = queue;
+  if (queue =! NULL){
+    if (idx <= 0){
+      for (int i = 0; Temp->pNext != NULL && i < idx; i++){
+        Temp = Temp->pNext;
+      }
+      return Temp->pData;
+    }
+    else {
+      for (int i = get_queue_size(queue); i + idx != 0; i--){
+        if (Temp->pNext == NULL) return NULL;
+        else Temp = Temp->pNext;
+      }
+      return Temp->pData;
+    }
+  }
+  else return NULL;
 }
 
 void * pop_from_queue (queue_t *queue)
