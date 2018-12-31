ce159b7b3f7af758cc326075b9443e15a9366975
Kadlec, Jakub <kadlej26@fel.cvut.cz>
2018-12-31 16:24:36 +0100

    Brute time ok, multiple fail

--- HW07/queue.c
+++ HW07/queue.c
@@ -3,6 +3,7 @@
 queue_t * pHead = NULL;   //Ukazatel na na posledy zapsany clen
 queue_t * pTail = NULL;   //Ukazatel na na posledy popnuty clen
 int cap;
+int size;
 int popStop = 0;
 queue_t * create_queue(int capacity){
   cap = capacity;
@@ -14,13 +15,13 @@ queue_t * create_queue(int capacity){
     for (int i = 0; i < (capacity - 1); i++){
       pCurrentStruct->pNext = (queue_t *)malloc(sizeof(queue_t));
       pCurrentStruct->pData = NULL;
-      pCurrentStruct->popped = false;
+      pCurrentStruct->popped = true;
       pCurrentStruct = pCurrentStruct->pNext;
     }
   }
   pCurrentStruct->pNext = pFirst;
   pCurrentStruct->pData = NULL;
-  pCurrentStruct->popped = false;
+  pCurrentStruct->popped = true;
   pHead = pFirst;
   pTail = pFirst;
   return pFirst;
@@ -31,45 +32,35 @@ bool push_to_queue (queue_t *queue, void * data)
   if (pHead != NULL){
     pHead->pData = data;
     pHead->popped = false;
-    printf ("%d ",(int) pHead);
-    printf ("%d\n",* ((int *) data));
-
     pHead = pHead->pNext;
+    size++;
     return true;
   }
   return false;
 }
 
 int get_queue_size(queue_t *queue){
-  queue_t * Temp = pTail;
-  int i = 0;
-  while (1){
-    if (Temp->popped == false) i++;
-    Temp = Temp->pNext;
-    if (Temp == pTail) break;
-  }
-  return i;
-  //printf("num %d ", numOfElements);
+  return size;
 }
 
 void* get_from_queue(queue_t *queue, int idx){
   queue_t * Temp = pTail;
-  if (pTail != NULL){
-    if (idx >= 0){
-      for (int i = 0; Temp->pNext != NULL && i < idx; i++){
-        Temp = Temp->pNext;
-      }
-      return Temp->pData;
-    }
-    else {
-      for (int i = get_queue_size(pTail); i + idx != 0; i--){
-        if (Temp->pNext == NULL) return NULL;
-        else Temp = Temp->pNext;
-      }
-      return Temp->pData;
-    }
+  if (abs(idx) >= cap){
+    return NULL;
   }
-  else return NULL;
+  if (idx >= 0){
+    for (int i = 0; Temp->pNext != NULL && i < idx; i++){
+      Temp = Temp->pNext;
+    }
+    return Temp->pData;
+  }
+  else {
+    for (int i = cap; i + idx != 0; i--){
+      if (Temp->pNext == NULL) return NULL;
+      else Temp = Temp->pNext;
+    }
+    return Temp->pData;
+  }
 }
 
 void * pop_from_queue (queue_t *queue)
@@ -79,28 +70,19 @@ void * pop_from_queue (queue_t *queue)
     pTail->pData = NULL;
     pTail->popped = true;
     pTail = pTail->pNext;
+    size--;
     return (int*) pLastVoid;
   }
   else return NULL;
 }
 
 void delete_queue(queue_t *queue){
-  queue_t * Temp = pTail->pNext;
-  pTail->pNext = NULL;
-  //printf ("%d\n",(int) Temp);
-  //printf ("%d\n",(int) *((int *) Temp->pData));
-  int i = 0;
+  queue_t * Temp = queue->pNext;
+  queue->pNext = NULL;
   while(Temp->pNext != NULL){
-    printf ("%d ", i);
-    printf ("%d",(int) Temp);
-    printf (" X %d\n",(int) pTail);
-    Temp = Temp->pNext;
+    queue = Temp->pNext;
     free (Temp);
-    i++;
+    Temp = queue;
   }
   free (Temp);
 }
-/*
-printf ("%d",(int) Temp);
-printf (" X %d\n",(int) pTail);
-printf ("%d\n", i);*/
